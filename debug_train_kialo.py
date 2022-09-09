87e98d1986f7a45cf693ecb528bba11f20e2a1a2
Iman Jundi <iman.jundi@gmail.com>
2022-09-09 17:59:30 +0200

    fix: url cleanup

--- debug_train_kialo.py
+++ debug_train_kialo.py
@@ -7,11 +7,10 @@ sys.argv += ['--debug_maps_size', '10']
 sys.argv += '--model_name_or_path sentence-transformers/paraphrase-albert-small-v2'.split(' ')
 sys.argv += '--hard_negatives False'.split(' ')
 sys.argv += '--lang english'.split(' ')
-sys.argv += '--use_dev True'.split(' ')
+# sys.argv += '--use_dev True'.split(' ')
 
-# sys.argv += '--eval_model_name_or_path sentence-transformers/paraphrase-multilingual-mpnet-base-v2'.split(' ')
-sys.argv += '--eval_model_name_or_path sentence-transformers/paraphrase-albert-small-v2'.split(' ')
-sys.argv += '--eval_not_trained True --do_train False'.split(' ')
+# sys.argv += '--eval_model_name_or_path sentence-transformers/paraphrase-albert-small-v2'.split(' ')
+# sys.argv += '--eval_not_trained True --do_train False'.split(' ')
 
 # sys.argv += '--rerank True'.split(' ')
 
--- util.py
+++ util.py
@@ -2,11 +2,6 @@ import re
 
 
 def remove_url_and_hashtags(text):
-    pattern = r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))'
-    match = re.findall(pattern, text)
-    for m in match:
-        url = m[0]
-        text = text.replace(url, '')
-    text = text.replace("()", "")
+    text = re.sub(r'\[([^\]]+)\]\(http[^\)]+\)', r'\1', text)
     text = text.replace("#", "")
     return text
