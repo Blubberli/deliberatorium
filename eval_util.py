e71a6964683a86c466c8bbf60874c286d31b9e74
Iman Jundi <iman.jundi@gmail.com>
2022-09-13 16:12:23 +0200

    eval only leafs

--- eval_util.py
+++ eval_util.py
@@ -6,25 +6,31 @@ from rerank_evaluation import RerankEvaluation
 METRICS = ['mrr', 'p5', 'p1', 'dist']
 
 
-def evaluate_map(encoder_mulitlingual, argument_map, node_types, max_candidates=0, cross_encoder=None):
+def evaluate_map(encoder_mulitlingual, argument_map, node_types, max_candidates=0, cross_encoder=None,
+                 eval_types=None):
+    if eval_types is None:
+        eval_types = ['only_leafs_limited_types']
     results = {}
     node_results = {}
     print(f'eval {argument_map.id} {len(argument_map.all_children)=} \n{argument_map.name}')
     encoder_mulitlingual.encode_argument_map(argument_map)
 
-    print("default setting: all nodes are evaluated, all nodes are considered as candidates")
     eval_args = {'argument_map': argument_map, 'only_leafs': False, 'max_candidates': max_candidates}
     if cross_encoder:
         eval_args['cross_encoder'] = cross_encoder
-    results['all'], node_results['all'] = eval_one(eval_args)
+    if 'all' in eval_types:
+        print("default setting: all nodes are evaluated, all nodes are considered as candidates")
+        results['all'], node_results['all'] = eval_one(eval_args)
 
-    print("only check for leaf nodes")
     eval_args['only_leafs'] = True
-    results['only_leafs'], node_results['only_leafs'] = eval_one(eval_args)
+    if 'only_leafs' in eval_types:
+        print("only check for leaf nodes")
+        results['only_leafs'], node_results['only_leafs'] = eval_one(eval_args)
 
-    print("only leaf nodes and only issues and ideas as parents")
     eval_args['candidate_node_types'] = node_types
-    results['only_leafs_limited_types'], node_results['only_leafs_limited_types'] = eval_one(eval_args)
+    if 'only_leafs_limited_types' in eval_types:
+        print("only leaf nodes and only issues and ideas as parents")
+        results['only_leafs_limited_types'], node_results['only_leafs_limited_types'] = eval_one(eval_args)
 
     return results, node_results
 
